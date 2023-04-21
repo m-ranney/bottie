3110ea8a34d69945b8d82f12e536e1afb9e9cdb9
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-21 20:15:00 +0000

    Create_event page & route from home page

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from google_calendar import create_oauth_flow, calendar_bp
 app = Flask(__name__)
 app.register_blueprint(calendar_bp, url_prefix='/calendar')
 
-
+# Route to the Home page
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# Route to the Create Event page
+@app.route('/create')
+def create():
+    return render_template('create_event.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
