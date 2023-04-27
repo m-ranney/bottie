8261a3a4e7f0c94cfcb8a3fe20d6389f7f294c3c
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-27 19:30:37 +0000

    Update steps prompt

--- steps.py
+++ steps.py
@@ -17,7 +17,7 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 def generate_subtasks(prompt: str) -> List[str]:
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=f"Please provide a list of detailed subtasks required to complete the task: {prompt}",
+        prompt=f"Please provide a list of detailed subtasks required to complete the task: {prompt}. Be as descriptive as possible and return your response in a bulleted list format. Be creative in your response and in general lean towards using modern technology solutions to make the subtasks as efficient as possible.",
         temperature=0.7,
         max_tokens=100,
         top_p=1,
