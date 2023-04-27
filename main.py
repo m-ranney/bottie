cc22efb96f89cb8b36cb5ac6fd7edfb5bb026614
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-27 19:27:16 +0000

    Create steps path and steps.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(calendar_bp, url_prefix='/calendar')
+app.register_blueprint(steps_bp)
 app.secret_key = os.environ['FLASK_SECRET_KEY']
 
 # Route to the Home page
@@ -13,10 +14,17 @@ def home():
     event_json = session.pop('event_json', None)  # Get the event_json from the session and remove it
     return render_template('index.html', success_message=success_message, event_json=event_json)
 
+
 # Route to the Create Event page
 @app.route('/create')
 def create():
     return render_template('create_event.html')
+
+
+# Route to the Step by Step page
+@app.route('/steps')
+def steps():
+    return render_template('steps.html')
 
 
 if __name__ == '__main__':
---
+++ steps.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+import openai
+import os
+from supabase_py import create_client, Client
+from typing import List, Dict
+
+steps_bp = Blueprint('steps_bp', __name__, template_folder='templates')
+
+# Initialize Supabase client
+supabase_url = os.environ['https://yoelhlhpvqxcdarwuool.supabase.co']
+supabase_key = os.environ['SUPABASE_KEY']
+supabase: Client = create_client(supabase_url, supabase_key)
+
+# Configure OpenAI API
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+def generate_subtasks(prompt: str) -> List[str]:
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Please provide a list of detailed subtasks required to complete the task: {prompt}",
+        temperature=0.7,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        n=1,
+    )
+
+    subtasks_raw = response.choices[0].text.strip()
+    subtasks = subtasks_raw.split("\n")
+    return [subtask.strip() for subtask in subtasks if subtask.strip()]
+
+@steps_bp.route('/steps', methods=['GET'])
+def steps():
+    tasks = supabase.table('tasks').select('*').execute()
+    return render_template('steps.html', tasks=tasks)
+
+@steps_bp.route('/steps', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    subtasks = generate_subtasks(task)
+
+    task_id = supabase.table('tasks').insert({'task': task, 'subtasks': subtasks}).execute()['id']
+
+    return redirect(url_for('steps_bp.steps'))
+
+@steps_bp.route('/update_status/<int:task_id>', methods=['POST'])
+def update_status(task_id):
+    new_status = request.form.get('status') == '1'
+    supabase.table('tasks').update({'status': new_status}).eq('id', task_id).execute()
+    return redirect(url_for('steps_bp.steps'))
