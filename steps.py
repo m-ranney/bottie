35143812bdb1143a2a5763ac92d7fc2451945bb5
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-28 20:34:01 +0000

    Sync tasks and sub tasks with supabase

--- steps.py
+++ steps.py
@@ -3,6 +3,7 @@ import openai
 import os
 from supabase_py import create_client, Client
 from typing import List, Dict
+from asyncio import get_event_loop
 
 steps_bp = Blueprint('steps_bp', __name__)
 
@@ -20,15 +21,23 @@ def steps():
     if request.method == 'POST':
         task_input = request.form.get('task_input')
         response = generate_subtasks(task_input)
-        subtasks_json = response.get('choices')[0].get('text')
-        return render_template('steps.html', subtasks_json=subtasks_json)
-    return render_template('steps.html')
+        subtasks_text = response.get('choices')[0].get('text')
+        subtasks_list = subtasks_text.strip().split("\n")
+        
+        task_id = get_event_loop().run_until_complete(insert_task(task_input))
+        get_event_loop().run_until_complete(insert_subtasks(task_id['id'], subtasks_list))
+        
+        tasks, subtasks = get_event_loop().run_until_complete(get_tasks_and_subtasks())
+        return render_template('steps.html', tasks=tasks, subtasks=subtasks)
+
+    tasks, subtasks = get_event_loop().run_until_complete(get_tasks_and_subtasks())
+    return render_template('steps.html', tasks=tasks, subtasks=subtasks)
 
 
 def generate_subtasks(task_input):
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=f"Please provide a list of detailed subtasks required to complete the task: {task_input}. Be as descriptive as possible and return your response in a bulleted list format. Be creative in your response and in general lean towards using modern technology solutions to make the subtasks as efficient as possible.",
+        prompt=f"Please provide a list of detailed subtasks required to complete the task: {task_input}. Each subtask should have a concrete start and finishing point. Be as descriptive as possible and return your response in a bulleted list format. Be creative in your response and in general lean towards using modern technology solutions to make the subtasks as efficient as possible.",
         temperature=0.7,
         max_tokens=200,
         top_p=1,
@@ -37,6 +46,8 @@ def generate_subtasks(task_input):
         n=1,
     )
     return response
+
+
 
 
 
