2c2ce94298803860ad30f1b80ee89192f3fe04fe
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-28 18:09:18 +0000

    Simplify output

--- steps.py
+++ steps.py
@@ -4,7 +4,7 @@ import os
 from supabase_py import create_client, Client
 from typing import List, Dict
 
-steps_bp = Blueprint('steps_bp', __name__, template_folder='templates')
+steps_bp = Blueprint('steps_bp', __name__)
 
 # Initialize Supabase client
 supabase_url = os.environ['SUPABASE_URL']
@@ -14,12 +14,23 @@ supabase: Client = create_client(supabase_url, supabase_key)
 # Configure OpenAI API
 openai.api_key = os.environ['OPENAI_API_KEY_CE']
 
-def generate_subtasks(prompt: str) -> List[str]:
+# Calls task input from form on steps.html
+@steps_bp.route('/steps', methods=['GET', 'POST'])
+def steps():
+    if request.method == 'POST':
+        task_input = request.form.get('task_input')
+        response = generate_subtasks(task_input)
+        subtasks_json = response.get('choices')[0].get('text')
+        return render_template('steps.html', subtasks_json=subtasks_json)
+    return render_template('steps.html')
+
+
+def generate_subtasks(task_input):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=f"Please provide a list of detailed subtasks required to complete the task: {prompt}. Be as descriptive as possible and return your response in a bulleted list format. Be creative in your response and in general lean towards using modern technology solutions to make the subtasks as efficient as possible.",
         temperature=0.7,
-        max_tokens=100,
+        max_tokens=200,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
@@ -27,11 +38,6 @@ def generate_subtasks(prompt: str) -> List[str]:
     )
     return response
 
-@steps_bp.route('/subtasks', methods=['POST'])
-def show_subtasks():
-    prompt = request.form['task']  # get the task input from the form
-    subtasks = generate_subtasks(prompt)
-    subtasks_list = subtasks.choices[0].text.split('\n')  # convert the response to a list of subtasks
-    return render_template('steps.html', prompt=prompt, subtasks=subtasks_list)
+
 
 
