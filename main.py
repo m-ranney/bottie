8345e55339ecd4f947513f36f3a6dfe681135b00
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-05-02 19:24:48 +0000

    Bug fixes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from google_calendar import create_oauth_flow, calendar_bp
 from steps import steps_bp
 from meals import meal_plan_bp
 import os
+import openai
 
 app = Flask(__name__)
 app.register_blueprint(calendar_bp, url_prefix='/calendar')
