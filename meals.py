8ba2c0bb0bcc2771000bb395e3c2a6a64566b7e3
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-05-04 21:00:28 +0000

    Prompt change to gpt3.5

--- meals.py
+++ meals.py
@@ -25,20 +25,18 @@ def meal_plan():
     if request.method == 'POST':
         meal_goal = request.form.get('meal_goal')
         response = generate_meal_plan(meal_goal)
-        meal_plan_text = response.get('choices')[0].get('text').strip()
+        meal_plan_text = response['choices'][0]['message']['content'].strip()
         meal_plan_dict = meal_plan_to_dict(meal_plan_text)
     return render_template('meal_plan.html', meal_plan_dict=meal_plan_dict, meal_plan_text=meal_plan_text)
 
 def generate_meal_plan(meal_goal):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Please provide a meal plan with a focus on {meal_goal} meals. Suggest multiple meals for breakfast, lunch, and dinner. Suggest at least 10 meals. Be as descriptive as possible. Include meals using the following format template, with one meal per line, for your response:---BEGIN FORMAT TEMPLATE---(Meal Type): (Meal). (Meal Type): (Meal).---END FORMAT TEMPLATE--- An example is 'Breakfast: Greek yogurt with blueberries and granola. \n Breakfast: Protein bar and a banana with coffee. \n Lunch: Kale salad with roasted chicken, dried cranberries and toasted walnuts. /n Dinner: Grilled salmon with roasted Brussels sprouts and roasted sweet potato.' Add some variety to the meal suggestions. Apply the ability to buy ingredients that can be used in multiple selected dishes for more efficient grocery shopping.",
-        temperature=1,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that provides meal plans."},
+            {"role": "user", "content": f"Please provide a meal plan with a focus on {meal_goal} meals. Suggest multiple meals for breakfast, lunch, and dinner. Suggest at least 10 meals. Be as descriptive as possible. Include meals using the following format template, with one meal per line, for your response:---BEGIN FORMAT TEMPLATE---(Meal Type): (Meal). (Meal Type): (Meal).---END FORMAT TEMPLATE--- An example is 'Breakfast: Greek yogurt with blueberries and granola. \n Breakfast: Protein bar and a banana with coffee. \n Lunch: Kale salad with roasted chicken, dried cranberries and toasted walnuts. /n Dinner: Grilled salmon with roasted Brussels sprouts and roasted sweet potato.' Add some variety to the meal suggestions. Apply the ability to buy ingredients that can be used in multiple selected dishes for more efficient grocery shopping."}
+        ],
         max_tokens=400,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        n=1,
     )
     return response
 
